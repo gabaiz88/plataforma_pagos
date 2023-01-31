f9ea6aaef1f26c124a99d7b007035a6773b3bf2f
Gabriel Aizman <gabaiz88@gmail.com>
2023-01-31 17:57:27 -0300

    tranform card

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ let current = "";
 let cardNumber = [];
 let dateNumber = [];
 let cvvNumber = [];
+let valid_date = false;
 
 inputCard.addEventListener("keydown", (e) => {
     if(e.key === "Tab"){
@@ -42,26 +43,47 @@ inputDate.addEventListener("keydown", (e) => {
         return;
     }
     e.preventDefault();
+
     handleImput(maskDate, e.key, dateNumber);
     inputDate.value = dateNumber.join("");
 });
 
+inputCVV.addEventListener("keydown", (e) => {
+    if(e.key === "Tab"){
+        return;
+    }
+    e.preventDefault();
+    handleImput(maskCVV, e.key, cvvNumber);
+    inputCVV.value = cvvNumber.join("");
+});
+
 function handleImput(mask, key, arr){
     let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    if(key == "backspace" && arr.lenght > 0){
+
+    if(key === "Backspace" && arr.length > 0){
         arr.pop();
         return
     }
-    if(numbers.includes(key) && arr.lenght + 1 <= mask.lenght){
-        if(mask[arr.lenght] === "-" || mask[arr.lenght] === "/ "){
-            arr.push(mask[arr.lenght], key);
-            console.log("entro");
-            console.log(key);
+    if(numbers.includes(key) && ((arr.length + 1) <= mask.length)){
+        if(mask[arr.length] === "-" || mask[arr.length] === "/"){
+            arr.push(mask[arr.length], key);
         } else {
             arr.push(key);
-            console.log("entro");
-            console.log(key);
         }
     }
 }
 
+function validar_date() {
+    date = [];
+    date = dateNumber.join("");
+    var month = date[0] + date[1];
+    if(!(month < 01 || month > 12)){
+        valid_date = true;
+    }
+}
+
+const card = document.querySelector('.card')
+
+card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped');
+});
